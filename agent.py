58d782e1b70c3bf47eed1b6f11a2f4f4dc4a99c7
Zaire Ali <35949230+zaireali649@users.noreply.github.com>
2025-09-28 10:26:34 -0400

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,5 +1,5 @@
 import os
-from agents import Agent, Runner
+from agents import Agent, Runner, set_default_openai_key
 import asyncio
 from dotenv import load_dotenv
 
@@ -9,27 +9,36 @@ async def generate_tasks(task_generator, goal):
     return result.final_output
 
 
-async def main(task_generator):
-    user_goal = (
-        "I want a 1200 calorie meal from chick-fil-a. I have no dietary restrictions."
-    )
-    user_goal = "I want a 1200 calorie meal from chick-fil-a. I cannot have gluten."
-    user_goal = "I want a 1200 calorie meal from Mcdonald's. I cannot have pork."
+async def run_nutrition_agent(task_generator, user_goal):
     tasks = await generate_tasks(task_generator, user_goal)
     print(tasks)
 
 
-if __name__ == "__main__":
-    load_dotenv()  # Load variables from .env into the environment
-
-    OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-
-    with open("agent_prompt.txt", "r") as file:
-        prompt = file.read()
-
-    task_generator = Agent(
+def get_task_generator(prompt):
+    return Agent(
         name="Task Generator",
         instructions=f'"""{prompt}"""',
     )
 
-    asyncio.run(main(task_generator))
+
+async def main():
+    load_dotenv()  # Load variables from .env into the environment
+
+    set_default_openai_key(os.environ["OPENAI_API_KEY"])
+
+    with open("prompts/agent_prompt.txt", "r") as file:
+        prompt = file.read()
+
+    task_generator = get_task_generator(prompt)
+
+    user_goals = [
+        "I want a 1200 calorie meal from chick-fil-a. I have no dietary restrictions.",
+        "I want a 1200 calorie meal from chick-fil-a. I cannot have gluten.",
+        "I want a 1200 calorie meal from Mcdonald's. I cannot have pork.",
+    ]
+    for user_goal in user_goals:
+        await run_nutrition_agent(task_generator, user_goal)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
